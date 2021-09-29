7be633b2048ec0bf55409bbd485904431b3eb92f
Malysh Alexey <malyshitof@gmail.com>
2021-09-29 03:07:08 +0300

    Add class in new Branch

---
+++ src/main/java/data/Person.java
@@ -0,0 +1,25 @@
+package data;
+
+public class Person extends BaseEntity{
+    public Person(String id) {
+        super(id);
+    }
+    private String firstName;
+    private String secondName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+}
